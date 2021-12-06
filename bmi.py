dba3b86f0b28280d79fa509a4256c0b5064bcef7
camryray <95621069+camryray@users.noreply.github.com>
2021-12-06 20:12:39 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -2,16 +2,19 @@ import streamlit as st
 
 def BMI(w, h):
     return w/(h*h)
-w = float(st.text_input('請輸入體重(KG)？'))
-h = float(st.text_input('請輸入身高(M)？'))
-bmi = BMI(w, h)
-# print('BMI為', bmi)
-st.write('BMI為', bmi)
-if (bmi < 18):
-    st.write('體重過輕')
-elif (bmi < 24):
-    st.write('體重正常')
-elif (bmi < 27):
-    st.write('體重過重')
-else:
-    st.write('體重肥胖')
+#w = float(input('請輸入體重(KG)?'))
+w = float(st.number_input('請輸入體重(KG)？'))
+h = float(st.number_input('請輸入身高(M)？'))
+comfirm_input = st.button('輸入確認')
+if comfirm_input:
+   bmi = BMI(w, h)
+   # print('BMI為', bmi)
+   st.write('BMI為', bmi)
+   if (bmi < 18):
+       st.write('體重過輕')
+   elif (bmi < 24):
+       st.write('體重正常')
+   elif (bmi < 27):
+       st.write('體重過重')
+   else:
+       st.write('體重肥胖')
